33e81772eb5cfe9ad930d0932565504798f4e232
Arnab Pramanik <165245105+Arnab141@users.noreply.github.com>
2026-04-20 15:21:27 +0530

    Update App.tsx

--- travel-buddy-connect-main/src/App.tsx
+++ travel-buddy-connect-main/src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter , Routes, Route } from "react-router-dom";
+import { BrowserRouter  , Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Auth from "./pages/Auth";
@@ -20,7 +20,7 @@ import ChatWidget from "./components/ChatWidget";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <HashRouter >
+  <BrowserRouter  >
     <UserProvider>
       <QueryClientProvider client={queryClient}>
         <TooltipProvider>
@@ -45,7 +45,7 @@ const App = () => (
         </TooltipProvider>
       </QueryClientProvider>
     </UserProvider>
-  </HashRouter >
+  </BrowserRouter  >
 );
 
 export default App;
